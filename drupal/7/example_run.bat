c72320b7ced353d4fd9f47e3399fa116cc3a4060
Fred Tyre <ftyre76@gmail.com>
2022-05-13 10:10:02 -0500

    Adding example_run.bat for d7

---
+++ drupal/7/example_run.bat
@@ -0,0 +1,11 @@
+echo OFF
+set D7ET_CURR_SITE_NAME=new_d7_folder
+set D7ET_CURR_DB_HOST=localhost
+set D7ET_CURR_DB_PORT=3306
+set D7ET_CURR_DB_USER=
+set D7ET_CURR_DB_PASS=
+set D7ET_CURR_DB_NAME=
+echo ON
+
+exporting website %D7ET_CURR_SITE_NAME% to output\%D7ET_CURR_SITE_NAME%
+python src\d7exportTaxonomy.py
